64bd3647118657ceffa4f1eb51cb1972f325b19e
angel melendez <angelmelen88@gmail.com>
2023-07-24 17:21:02 -0600

    consumo de api

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,31 +1,31 @@
 
 
-export const Card = () => {
+export const Card = (data) => {
+  console.log(data)
   return (
-    <div className=" max-w-sm bg-white rounded-lg overflow-hidden shadow-md m-5  cursor-pointer">
-      <figure className="relative mb-2 w-full h-4/5">
-        <span className="absolute bottom-0 left-0 bg-slate-600 rounded-lg text-black text-sm px-3"> tipo </span>
-        <img className="h-48 w-full object-cover" src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/133.png" alt="pokemon" />
+    <div className=" max-w-sm h-96 bg-white rounded-lg overflow-hidden shadow-md m-5  cursor-pointer">
+      <figure className="relative mb-2 w-full h-3/5">
+        <span className="absolute bottom-0 left-5 bg-slate-600 rounded-lg text-black text-sm px-3"> {data.data.category.name} </span>
+        <img className="h-56 w-full object-cover" src={data.data.images[0]} alt="pokemon" />
         <div className="absolute top-0 right-0 flex justify-center items-center bg-slate-500 w-6 h-6 rounded-s-full">
           +
         </div>
       </figure>
 
-      <div className="px-4 py-2 ">
-        <h3 className="text-lg font-semibold text-gray-900">Name: Eevee</h3>
-        <p className="text-gray-500 text-sm font-light">ID: 23</p>
-        <p className="text-gray-500 text-sm font-light">Habilidades: ser el mejor</p>
+      <div className="px-4">
+        <h3 className="text-base font-normal text-gray-900">Name: {data.data.title}</h3>
+        <p className="text-gray-500 text-sm font-light">ID: {data.data.category.id}</p>
       </div>
 
-      <div className="grid grid-cols-2 gap-4 px-4 py-2">
+      <div className="grid grid-cols-2 gap-4 px-4 py-1">
         {/* Pequeñas cards alternativas */}
         <div className="bg-blue-200 rounded-lg p-2">
-          Contenido de la card 1
+        price: {data.data.price}
         </div>
         <div className="bg-green-200 rounded-lg p-2">
-          Contenido de la card 2
+        description: {data.data.category.name}
         </div>
-        {/* Puedes agregar más pequeñas cards aquí */}
+       
       </div>
     </div>
   );
--- src/page/Home/Home.jsx
+++ src/page/Home/Home.jsx
@@ -1,11 +1,29 @@
+import { useState, useEffect } from "react"
 import { Layout } from "../../Components/Layout/Layout"
 import { Card } from "../../Components/Card/Card"
 
 export function Home() {
+  const [items, setItems] = useState(null)
+
+  useEffect(() => {
+    fetch('https://api.escuelajs.co/api/v1/products')
+      .then(response => response.json())
+      .then(data => setItems(data))
+  }, [])
+
+  
+
   return (
     <Layout>
       Home aqui las Card
-      <Card></Card>
+      <div className='grid gap-2 grid-cols-3 w-full max-w-screen-lg'>
+        {
+          items?.map(item => (
+            <Card key={item.id} data={item} />
+          ))
+          }     
+
+      </div>
     </Layout>
   )
 }
