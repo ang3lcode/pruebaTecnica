540de9021cc97f0e8678b53cd4256a0783d04564
angel melendez <angelmelen88@gmail.com>
2023-07-24 11:41:21 -0600

    inicio del proyecto

---
+++ src/page/App/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import { Navbar } from '../../template/Navbar'
+import { Footer } from '../../template/Footer'
+import { Card } from '../componente/Card'
+
+function App() {
+  
+
+  return (
+    <>
+      <Navbar/>
+      <Card></Card>
+      <Footer/>
+
+    </>
+  )
+}
+
+export default App
