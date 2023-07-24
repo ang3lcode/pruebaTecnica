fc15680c0721731e7ef8dce0b01ab9970cdf89a8
angel melendez <angelmelen88@gmail.com>
2023-07-24 11:42:37 -0600

    header y footer

---
+++ src/template/Footer.jsx
@@ -0,0 +1,9 @@
+
+export const Footer = () => {
+  return (
+    <footer className="bg-gray-200 py-4 text-center fixed bottom-0 w-full">
+      <p className="text-gray-600">practica tecnica, para desarrollador web</p>
+    </footer>
+  );
+};
+
---
+++ src/template/Navbar.jsx
@@ -0,0 +1,26 @@
+
+
+
+export const Navbar = () => {
+  return (
+    <nav className="bg-blue-500 p-4">
+      <div className="container mx-auto flex justify-between items-center">
+
+        <a href="#" className="text-white text-3xl font-semibold">Pokemon</a>
+        <div className="hidden md:flex space-x-4">
+          <a href="#" className="text-white">Inicio</a>
+          <a href="#contacto" className="text-white">Contacto</a>
+        </div>
+        <div className="md:hidden flex">
+          {/* Botón de menú para dispositivos móviles */}
+          <button className="text-white">
+            <svg className="h-6 w-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+              {/* <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7"></path> */}
+            </svg>
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
