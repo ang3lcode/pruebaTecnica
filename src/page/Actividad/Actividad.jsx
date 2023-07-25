ce867b3aff7a570875fc369776311904c4bab20c
angel melendez <angelmelen88@gmail.com>
2023-07-24 22:34:12 -0600

    manejo del array, de la segunda actividad y pagina de  error

--- src/page/Actividad/Actividad.jsx
+++ src/page/Actividad/Actividad.jsx
@@ -1,9 +1,71 @@
 import { Layout } from "../../Components/Layout/Layout"
+import { useState } from 'react';
 
 export function Actividad() {
+
+      // Arreglos
+  const arregloOriginal = [
+    {
+      aseguradora: "AFIRME",
+      cotizacion: {
+        cliente: {
+          tipoPersona: "fisica",
+          nombre: "prueba",
+          apellidoPat: "prueba",
+          apellidoMat: "prueba",
+          rfc: "",
+          fechaNacimiento: "01-01-2005",
+          ocupacion: "",
+          curp: "",
+          direccion: {
+            calle: "oriente 945",
+            noExt: "410",
+            noInt: "021",
+            colonia: "prueba",
+            codPostal: "56618",
+            poblacion: "mexico",
+            ciudad: "cdmx",
+            pais: "mexico"
+          },
+          edad: "18",
+          genero: "MASCULINO",
+          telefono: "",
+          email: ""
+        }
+      }
+    }
+  ];
+
+  const [arregloModificado, setArregloModificado] = useState([...arregloOriginal]);
+
+  // Funciones para mostrar los arreglos en pantalla
+  const mostrarArregloOriginal = () => {
+    setArregloModificado([...arregloOriginal]);
+  };
+
+  const mostrarArregloModificado = () => {
+    const arregloModificadoCopia = JSON.parse(JSON.stringify(arregloOriginal));
+    arregloModificadoCopia[0].aseguradora = "AXA Seguros";
+    arregloModificadoCopia[0].cotizacion.cliente.nombre = "Juan";
+    arregloModificadoCopia[0].cotizacion.cliente.apellidoPat = "Pérez";
+    arregloModificadoCopia[0].cotizacion.cliente.apellidoMat = "García";
+    arregloModificadoCopia[0].cotizacion.cliente.genero = "FEMENINO";
+    arregloModificadoCopia[0].cotizacion.cliente.direccion.colonia = "Centro";
+    arregloModificadoCopia[0].cotizacion.cliente.telefono = "12345 3456";
+    arregloModificadoCopia[0].cotizacion.cliente.email= "ejemplo@correo.com";
+    setArregloModificado(arregloModificadoCopia);
+  };
   return (
     <Layout>
-      Actividad
+      <div className="max-w-lg mx-auto my-8">
+      <div className="flex justify-center mb-4">
+        <button className="px-4 py-2 mr-4 bg-blue-500 text-white rounded" onClick={mostrarArregloOriginal}>Mostrar Arreglo Original</button>
+        <button className="px-4 py-2 bg-green-500 text-white rounded" onClick={mostrarArregloModificado}>Mostrar Arreglo Modificado</button>
+      </div>
+      <pre className="bg-gray-200 p-4 rounded">
+        {JSON.stringify(arregloModificado, null, 2)}
+      </pre>
+    </div>
     </Layout>
   )
 }
--- src/page/Contacto/Contacto.jsx
+++ src/page/Contacto/Contacto.jsx
@@ -5,14 +5,14 @@ export function Contacto() {
     <Layout>
         <div className="flex items-center justify-center w-4/6 h-5/6 bg-gray-100">
             <div className="bg-white shadow-xl rounded-lg p-8 flex">
-                <div className="w-2/3 pr-8">
+                <div className="w-5/6 p-40 pr-8">
                     <h2 className="text-4xl font-semibold mb-6">Luis Angel Melendez</h2>
                     <p className="text-lg mb-4">Desarrollador web</p>
                     <p className="text-lg mb-4">correo electronico: angelmelen88@gmail.com</p>
 
                    
                 </div>
-                <div className="w-1/3">
+                <div className="w-2/3">
                     <img className="h-full w-full object-cover rounded-lg" src="https://blog.uber-cdn.com/cdn-cgi/image/width=2161,height=1081,quality=80,onerror=redirect,format=auto/wp-content/uploads/2018/12/MX_Vive-como-nunca-antes-Ciudad-de-M%C3%A9xico-con-estos-X-lugares-de-inter%C3%A9s.jpg" alt="Imagen de la tarjeta" />
                 </div>
             </div>
